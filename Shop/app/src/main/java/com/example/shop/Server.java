f72048e091e4c8b6b68764dd9645c9dd48cf1757
minhquoc5599 <minhquoc5599@gmail.com>
2019-11-16 13:06:11 +0700

    add Server and CheckConect

---
+++ Shop/app/src/main/java/com/example/shop/Server.java
@@ -0,0 +1,17 @@
+package com.example.shop;
+
+public class Server {
+    public static String localhost ="?";
+    public static String LoaiSanPham ="http://"+ localhost +"/server/getloaisanpham.php";
+    public static String SanPhamMoiNhat="http://"+ localhost+ "/server/getsanphammoinhat.php";
+    public static String SanPham = "http://"+ localhost+ "/server/getsanpham.php";
+    public static String SanPhamFlashSale = "http://"+ localhost+ "/server/getsanphamflashsale.php";
+    public static String ThongTinKhachHang ="http://"+ localhost +"/server/thongtinkhachhang.php";
+    public static String ChiTietDonHang ="http://"+ localhost +"/server/chitietdonhang.php";
+    public static String signup ="http://"+ localhost +"/server/signup.php";
+    public static String signin ="http://"+ localhost +"/server/signin.php";
+
+
+    // đường dẫn tự đặt, cái trên ví dụ, localHost điền vào
+
+}
