ab22fe7f8541ac868b83213d92f69fb7d7cb9d3f
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2019-11-18 14:16:59 +0700

    Add acc info, logout

--- Shop/app/src/main/java/com/example/shop/AccountActivity.java
+++ Shop/app/src/main/java/com/example/shop/AccountActivity.java
@@ -3,24 +3,106 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.shop.classoop.Navigation;
+import com.example.shop.classoop.Product;
+import com.example.shop.classoop.UserInfo;
+import com.example.shop.module.Server;
+import com.example.shop.module.SessionManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccountActivity extends AppCompatActivity {
 
     Toolbar toolbarAccount;
+    SessionManager sessionManager;
+    TextView tvMail;
+    TextView tvName;
+    TextView tvLogout;
+    ArrayList<UserInfo> userInfos;
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        toolbarAccount =findViewById(R.id.toolbarAccount);
+        sessionManager = new SessionManager(this);
 
+        toolbarAccount = findViewById(R.id.toolbarAccount);
+
+        tvMail = findViewById(R.id.tvMail);
+        tvName = findViewById(R.id.tvAccount);
+        id = sessionManager.GetUser();
+        tvLogout = findViewById(R.id.tvLogout);
+        userInfos = new ArrayList<>();
+        String url = Server.getinfo + "?id=" + id;
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplication());
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i = 0; i < response.length();i++){
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                userInfos.add (new UserInfo(
+                                        jsonObject.getInt("id"),
+                                        jsonObject.getString("ten"),
+                                        jsonObject.getString("email")
+                                ));
+                                tvMail.setText(userInfos.get(0).getEmail());
+                                tvName.setText(userInfos.get(0).getTen());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+                ,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplication(), "product-kind false", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        requestQueue.add(jsonArrayRequest);
         actionBar();
-    }
+      //  GetInfo();
 
+        tvLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvLogout.setSelected(true);
+                sessionManager.SetLogin(false);
+                sessionManager.SetUser(-1);
+                sessionManager.Clear();
+                finish();
+            }
+        });
+    }
 
     private void actionBar()
     {
@@ -33,5 +115,11 @@ public class AccountActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+    private void GetInfo(){
+
+
     }
 }
